4de8f48b131f77941f1f540137b3f75e5db5bf21
Joseph <147633570+Joseph-0147@users.noreply.github.com>
2024-11-27 12:58:05 +0300

    Add files via upload

---
+++ AdvancedProgramming/src/Main.java
@@ -0,0 +1,30 @@
+import AssignmentSolutions.*;
+import java.util.Calendar;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        BankAccount account = new BankAccount(500);
+        Calendar date = Calendar.getInstance();   // Get the current date and time using Calendar
+
+        DepositTransaction deposit = new DepositTransaction(500, date);
+        WithdrawalTransaction withdrawal = new WithdrawalTransaction(600, date);
+
+        System.out.println("Initial balance: "+ "Ksh" + account.getBalance());   // Display the initial balance of the account
+
+        deposit.apply(account);    // Apply the deposit transaction to the account
+        deposit.printTransactionDetails();
+
+        try {    // Try to apply the withdrawal transaction to the account
+            withdrawal.apply(account);
+        } catch (InsufficientFundsException e) {  // Catch and display an error if there are insufficient funds for the withdrawal
+            System.out.println(e.getMessage());
+        } finally {
+            System.out.println("After withdrawal, balance: "+ "Ksh" + account.getBalance());
+        }
+
+        // Reversing withdrawal
+        withdrawal.reverse(account);
+    }
+}
